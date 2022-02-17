b5fce0618066b1ae2a3668b3dbd403d57c707687
josep <olmosescolatreball@gmail.com>
2022-02-17 18:10:50 +0100

    .

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import App from './App.vue'
 import { createAuth0 } from '@auth0/auth0-vue';
 
 
-createApp(App)    
-    .use(router)
-    .use(
-        createAuth0({
-            domain: "dev-y8fahynx.us.auth0.com",
-            client_id: "E1YmsI1kL7dlYpxw7cLUe9BVgUexPjzI",
-            redirect_uri: window.location.origin,
-            audience: "https://vue-project-gerardfp.vercel.app/api",
-            useRefreshTokens: true,
-            cacheLocation: 'localstorage'
-        })
-    )
-    .mount('#app');
+createApp(App)
+  .use(router)
+  .use(
+    createAuth0({
+      domain: "dev-9dydb7no.us.auth0.com",
+      client_id: "ZalYv6u0tlVlgUiaFnso3eCeKVkEfSTX",
+      redirect_uri: window.location.origin,
+      audience: "https://vue-project-gerardfp.vercel.app/api",
+      useRefreshTokens: true,
+      cacheLocation: "localstorage",
+    })
+  )
+  .mount("#app");
